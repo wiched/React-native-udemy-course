4d29a09dd7754280ae0b3619fdef23b98fdcdaad
Wiched <wiched@users.noreply.github.com>
2020-04-15 15:08:39 +0300

    initial commit

---
+++ rn-starter/src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => <Text style={styles.text}>HomeScreen</Text>;
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+});
+
+export default HomeScreen;
